ca453f3068394f66db1314fd1df88cb75022e514
luoyelun <luoyelun@outlook.com>
2020-04-24 12:22:30 +0800

    使用github接口登录，获得用户信息

--- src/main/java/com/community/controller/AuthorizeController.java
+++ src/main/java/com/community/controller/AuthorizeController.java
@@ -6,8 +6,11 @@ import com.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author luoyelun
@@ -26,7 +29,9 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
-                           @RequestParam("state") String state) {
+                           @RequestParam("state") String state,
+                           HttpServletRequest request,
+                           Model model) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -35,6 +40,13 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        return "index";
+        if (user != null) {
+            //登陆成功写session和cookie
+            request.getSession().setAttribute("user", user);
+            return "redirect:index";
+        } else {
+            //登录失败，重新登录
+            return "redirect:index";
+        }
     }
 }
